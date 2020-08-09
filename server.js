d522df3f443fc4a79afad3b383d626f5f88efc63
aalbarqi <ashjanalbarqi2@gmail.com>
2020-08-09 12:11:18 +0300

    edit

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const superagent = require('superagent');
+const pg = require('pg');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('./public'));
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).send('Homepage');
+});
+
+app.listen(PORT, () => {
+    console.log('Server is listening to port ', PORT);
+});
+
+app.all('*', (req, res) => {
+    res.status(500).send('Status 500: Sorry, something went wrong');
+});
