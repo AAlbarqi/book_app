42f066c95733e9df46d27523e3597f01dce1904c
aalbarqi <ashjanalbarqi2@gmail.com>
2020-08-10 22:23:25 +0300

    edit

---
+++ seed.sql
@@ -0,0 +1,17 @@
+INSERT INTO books (author,title,isbn,image_url,description,bookshelf) VALUES (
+  'Dune',
+  'Frank Herbert',
+  '9780441013593',
+  'https://books.google.com/books/content?id=B1hSG45JCX4C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  'Follows the adventures of Paul Atreides, the son of a betrayed duke given up for dead on a treacherous desert planet and adopted by its fierce, nomadic people, who help him unravel his most unexpected destiny.',
+  'Adventure'
+);
+
+INSERT INTO books (author,title,isbn,image_url,description,bookshelf) VALUES (
+  'The Fellowship of the Ring',
+  'John Ronald Reuel Tolkien',
+  '9780007522903',
+  'https://books.google.com/books/content?id=f3q6mwEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api',
+  'When they were first published, THE HOBBIT and THE LORD OF THE RINGS became instant classics. Treasured by readers young and old, these works of sweeping fantasy, steeped in unrivalled magic and otherworldliness have sold more than 150 million copies around the world. This new boxed set contains both titles and features brand new cover designs. It offers readers a new opportunity to discover Tolkien''s remarkable world of Middle-earth and to follow the complete story of Bilbo Baggins and the Hobbits'' part in the epic quest for the Ring beginning with Bilbo''s fateful visit from Gandalf and culminating in the dramatic climax between Frodo and Gollum atop Mount Doom',
+  'Fantasy'
+);       
--- server.js
+++ server.js
@@ -16,11 +16,18 @@ app.use(express.static('./public'));
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.render('./pages/index.ejs');
-});
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
 
+app.get('/', getSavedBooks);
+app.get('/searches/new', (req, res) => {
+  res.render('./pages/searches/new.ejs');
+});
 app.post('/searches', searchBook);
+app.post('/books', searchBooks);
+app.get('/book/:id', getBook);
+
 
 app.listen(PORT, () => {
   console.log('Server is listening to port ', PORT);
@@ -31,6 +38,40 @@ let handleError = (err, res) => {
 }
 app.all('*', handleError)
 
+function searchBooks(req,res){
+  console.log(req.body)
+  let {author,title,isbn,image_url,description,bookshelf} = req.body;
+  let SQL = 'INSERT into books(author,title,isbn,image_url,description,bookshelf) VALUES ($1, $2, $3, $4, $5, $6);';
+  let values = [author,title,isbn,image_url,description,bookshelf];
+
+  return client.query(SQL, values).then( ()=>{
+    SQL = 'SELECT * FROM books WHERE isbn = $1;';
+    values = [req.body.isbn];
+    return client.query(SQL,values).then( data => {
+      res.redirect(`/book/${data.rows[0].id}`)
+    }).catch(err => handleError(err,res));
+  })
+}
+
+function getBook (req,res){
+  let SQL = 'SELECT * FROM books WHERE id=$1;';
+  let values = [req.params.id];
+  client.query(SQL, values).then(data => {
+    res.render('pages/books/show', {book:data.rows[0]});
+  }).catch(err => handleError(err,res));
+}
+
+function getSavedBooks(req,res){
+  let SQL = 'SELECT * FROM books;';
+  return client.query(SQL).then(data => {
+    if(data.rowCount === 0){
+      res.render('pages/index');
+    } else {
+      res.render('pages/index', {bookList : data.rows});
+    }
+  }).catch(err => handleError(err,res));
+}
+
 function searchBook (req,res){
   let url = `https://www.googleapis.com/books/v1/volumes?q=${req.body.searchQuery}+${req.body.title ? 'intitle' : 'inauthor'}`;
   superagent.get(url).then(data => {
@@ -39,18 +80,21 @@ function searchBook (req,res){
       let title = book.volumeInfo.title;
       let author = book.volumeInfo.authors;
       let description = book.volumeInfo.description;
-      let img = book.volumeInfo.imageLinks.thumbnail;
-      books.push(new Book(title,author,img,description));
+      let image_url = book.volumeInfo.imageLinks.thumbnail;
+      let isbn = book.volumeInfo.industryIdentifiers[0].identifier;
+      books.push(new Book(title,author,image_url,description,isbn));
     });
     res.render('./pages/searches/show.ejs', {bookList:books});
   }).catch(err => handleError(err, res));
 }
 
 class Book {
-  constructor(title, author, img, description) {
+  constructor(title, author, image_url, description, isbn) {
     this.title = title || 'Not available';
     this.author = author || 'No Author available';
-    this.img = img.replace(/^(http:\/\/)/g,'https://') || 'https://i.imgur.com/J5LVHEL.jpg';
+    this.image_url = image_url.replace(/^(http:\/\/)/g,'https://') || 'https://i.imgur.com/J5LVHEL.jpg';
     this.description = description || 'No description';
+    this.isbn = isbn || 'No ISBN';
+    this.bookshelf = 'Not Available';
   }
 }
